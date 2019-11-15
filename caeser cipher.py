3c0776ac21b496aba332fc7db856636bc836f7f2
younusimran <57294532+younusimran@users.noreply.github.com>
2019-11-15 12:10:14 +0500

    Add files via upload
    Caeser ciphering of alphabets based on number of word shifting given

---
+++ caeser cipher.py
@@ -0,0 +1,47 @@
+message = input("Enter a message to encrypt - Only message in alphabet will be encrypted: ")
+
+cond = True
+while cond:
+    try:
+        shift = input("Enter number to use word shift for encrypting or type exit to quit: ")
+        if shift.upper() == "EXIT":
+            break
+        else:
+            shift = int(shift)
+            cond = False
+    except:
+        continue
+nm = ""
+count = 0
+for char in message:
+    if char.isalpha() and char.isupper():
+        if (ord(char)+shift) > 90: 
+            while count < shift:
+                count += 90 - 65 + 1
+                if count < shift:
+                    num = shift - count
+            count = 0
+            nm += chr((ord(char)+num))
+            num = 0
+            #nm += chr((ord("A") + shift-(90-ord(char)+1)))
+        else:
+            nm += chr(ord(char)+shift)
+    elif char.isalpha() and char.islower():
+        if (ord(char)+shift) > 122:
+            while count < shift:
+                count += 122 - 97 + 1
+                if count < shift:
+                    num = shift - count
+            count = 0
+            nm += chr((ord(char)+num))
+            num = 0
+            #nm += chr((ord("a") + shift-(122-ord(char)+1)))
+        else:
+            nm += chr(ord(char)+shift)
+    else:
+        nm += char
+
+
+print("The message was: ",message)
+print("Character shift for alphabets was: ",shift)
+print("Encrypted message: ",nm)
